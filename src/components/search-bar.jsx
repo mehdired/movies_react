42d7895ac2152ab95d1a20cb0d673483a03abaed
Mehdi Cheniki <mehdi.cheniki@gmail.com>
2018-02-20 15:09:49 +0100

    done (without redux)

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -6,20 +6,44 @@ class SearchBar extends React.Component {
 
     this.state = {
       searchText: '',
-      placeHolder: 'Tapez votre film...'
+      placeHolder: 'Tapez votre film...',
+      intervalRequest: 1000,
+      lockRequest: false
     }
 
     this.handleChange = this.handleChange.bind(this)
+    this.handleOnClick = this.handleOnClick.bind(this)
   }
 
   handleChange (event) {
     this.setState({searchText: event.target.value})
+    if (!this.state.lockRequest) {
+      this.setState({lockRequest: true})
+      setTimeout(() => {
+        this.search()
+      },
+    this.state.intervalRequest)
+    }
+  }
+
+  handleOnClick () {
+    this.search()
+  }
+
+  search () {
+    this.props.callback(this.state.searchText)
+    this.setState({lockRequest: false})
   }
 
   render () {
     return (
-      <div>
-        <input onChange={this.handleChange} placeholder={this.state.placeHolder} />
+      <div className='row'>
+        <div className='col-md-8 input-group'>
+          <input type='text' className='form-control input-lg' onChange={this.handleChange} placeholder={this.state.placeHolder} />
+          <span className='input-group-button'>
+            <button className='btn btn-secondary' onClick={this.handleOnClick}>GO</button>
+          </span>
+        </div>
       </div>
     )
   }
